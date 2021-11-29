37e576b84045805058d4aa28397150dcb1068f5c
hiven <mail@hiven.com>
2021-11-29 14:17:16 +0000

    Update config.py

--- config.py
+++ config.py
@@ -2,25 +2,23 @@
 
 import os 
 
-
 class Config:
     FLASK_ENV = 'development'
     DEBUG = False
     TESTING = False
     WTF_CSRF_ENABLED = True
-    SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'ccxcxx'
+#    SQLALCHEMY_TRACK_MODIFICATIONS = False
     
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.getenv('SECRET_KEY')
+#    SQLALCHEMY_DATABASE_URI = 'ccxcxx'
 
 class TestingConfig(Config):
     TESTING = True
     WTF_CSRF_ENABLED = False
-    MAIL_SUPPRESS_SEND = True
-    SQLALCHEMY_DATABASE_URI = os.getenv('SECRET_KEY')
+#    SQLALCHEMY_DATABASE_URI = os.getenv('SECRET_KEY')
 
 class ProductionConfig(Config):
     FLASK_ENV = 'production'
-    SQLALCHEMY_DATABASE_URI = os.getenv('SECRET_KEY')
+#    SQLALCHEMY_DATABASE_URI = os.getenv('SECRET_KEY')
