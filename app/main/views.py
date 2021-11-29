c5ab30fd382305b7ba37e0342449973506524d5d
hiven <mail@hiven.com>
2021-11-29 14:00:17 +0000

    Update views.py

--- app/main/views.py
+++ app/main/views.py
@@ -3,7 +3,6 @@ from flask import render_template, request, redirect, url_for, current_app, abor
 
 @main_blueprint.route('/')
 def index():
-    current_app.logger.info("Index page loading")
     return render_template('main/index.html')
 
 @main_blueprint.route('/admin')
