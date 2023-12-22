5f612a77a6d7037c56fea7819f03c33c18d0e73a
hiven <mail@hiven.com>
2023-12-22 20:49:40 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 import os 
 
 class Config:
@@ -8,17 +6,13 @@ class Config:
     TESTING = False
     WTF_CSRF_ENABLED = True
     SECRET_KEY = 'ccxcxx'
-#    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
+
 class DevelopmentConfig(Config):
     DEBUG = True
-#    SQLALCHEMY_DATABASE_URI = 'ccxcxx'
 
 class TestingConfig(Config):
     TESTING = True
     WTF_CSRF_ENABLED = False
-#    SQLALCHEMY_DATABASE_URI = os.getenv('SECRET_KEY')
 
 class ProductionConfig(Config):
     FLASK_ENV = 'production'
-#    SQLALCHEMY_DATABASE_URI = os.getenv('SECRET_KEY')
